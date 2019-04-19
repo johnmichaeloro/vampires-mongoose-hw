2e4594ee07f89bac6b982d0c555a94335e99f528
John Oro <johnmichaeloro@gmail.com>
2019-04-19 14:23:42 -0600

    added data into vampires collection

--- vampire_app/app.js
+++ vampire_app/app.js
@@ -4,12 +4,65 @@ const methodOverride = require('method-override');
 const bodyParser = require('body-parser');
 const vampire = require('./models/vampire');
 const vampireData = require('./populateVampires');
+const mongoose = require('mongoose');
 
 app.use(methodOverride('_method'));
 
 app.use(bodyParser.urlencoded({extended:false}));
 
 require('./db/db');
+
+//Added the vampire data to the vampire collection.
+/**
+vampire.collection.insertMany(vampireData, (err, data) => {
+  console.log("Added vampire data!!!");
+  mongoose.connection.close();
+})
+**/
+
+vampire.create({
+  name: "Brewhaha the Vampster",
+  hair_color: 'green',
+  eye_color: 'purple',
+  loves: ['blood', 'more blood'],
+  location: 'Cancun',
+  gender: 'm',
+  victims: 100,
+},
+  {
+    name: 'Glamping Queen Brazilionaire Karen',
+    hair_color: 'blondish',
+    eye_color: 'violet',
+    loves: ['glamping', 'biting necks'],
+    location: 'Rio de Janeiro',
+    gender: 'f',
+    victims: 43,
+  },
+  {
+    name: 'Groans of the Damned or just Bob',
+    hair_color: 'bald',
+    eye_color: 'brown',
+    loves: ['basements', 'dried blood', 'buckets of blood'],
+    location: 'below your feet',
+    gender: 'm',
+    victims: 428,
+  },
+  {
+    name: 'Pritzle Golunahnah',
+    hair_color: 'red',
+    eye_color: 'blue',
+    loves: ['blood', 'long walks by the morgue'],
+    location: 'behind you',
+    gender: 'f',
+    victims: 294,
+  }, (err, createdVampire) => {
+    if(err) {
+      console.log(err);
+    } else {
+      console.log(createdVampire);
+    }
+  }
+)
 
 /////////////////////////////////////////////////
 //Write your answers to add, query, update, remove, and Hungry for More below.
